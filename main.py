861af4eacd5cd1643a66ff6c48d39ab5aa2f41a2
Rivan-Permana <ansinar2003@gmail.com>
2025-12-03 03:57:42 +0700

    chore: add cors list for fe-prod url

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ fernet = Fernet(FERNET_KEY.encode()) if FERNET_KEY else None
 
 CORS_ORIGINS = os.getenv(
     "CORS_ORIGINS",
-    "http://127.0.0.1:5500,http://localhost:5500,http://localhost:5173,http://127.0.0.1:5173,https://convoinsight.vercel.app",
+    "http://127.0.0.1:5500,http://localhost:5500,http://localhost:5173,http://127.0.0.1:5173,https://convoinsight.vercel.app,https://convo-insight.vercel.app",
 ).split(",")
 
 DATASETS_ROOT = os.getenv("DATASETS_ROOT", os.path.abspath("./datasets"))
